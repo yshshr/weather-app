9d9cabc2007fa7c7e13a0cdae1792bdb1061b9d3
ls <65608254+yshshr@users.noreply.github.com>
2026-09-08 17:20:21 +0800

    创建api调用函数

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import "../components/my-component/style.css";
 
-console.log("hello, Odinite!");
+async function requestWeatherApi(locationInfo) {
+  let location;
+  if (locationInfo.latitude && locationInfo.longitude) {
+    location = locationInfo.latitude + "," + locationInfo.longitude;
+  } else if (location.city) {
+    location = location.city;
+  }
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&include=days&key=UBSC6YRVQFY7H598UF2CXCNJV&contentType=json`,
+    );
+    if (!response.ok) {
+      throw new Error("weather api return error!");
+    }
+    const resJoon = await response.json();
+    return resJoon;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function getWeatherInfo() {
+  let location = {
+    latitude: 31.3,
+    longitude: 120.6,
+  };
+  requestWeatherApi(location).then((resJson) => {
+    console.log(resJson);
+  });
+}
+
+getWeatherInfo();
